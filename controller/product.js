2fc2d6dd78ac3ccdd9bd3805749fae2467e03c71
Ar1f007 <muhammadarif9416@yahoo.com>
2022-08-04 22:56:45 +0600

    get products api

--- controller/product.js
+++ controller/product.js
@@ -15,3 +15,31 @@ exports.addProduct = async (req, res) => {
     return res.status(424).json({ message: 'Could not add product' });
   }
 };
+
+exports.getProducts = async (req, res) => {
+  let result;
+  let totalProducts;
+  const { search } = req.query;
+
+  let query = { $regex: search, $options: 'i' };
+
+  if (search) {
+    result = Product.find({ name: query });
+    totalProducts = await Product.countDocuments({ name: query });
+  } else {
+    result = Product.find();
+    totalProducts = await Product.countDocuments();
+  }
+
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 10;
+
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const numOfPages = Math.ceil(totalProducts / limit);
+
+  const products = await result.sort({ createdAt: -1 });
+
+  res.status(200).json({ products, numOfPages, totalProducts });
+};
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { addProduct } = require('../controller/product');
+const { getProducts, addProduct } = require('../controller/product');
 const router = express.Router();
 
+router.get('/products', getProducts);
 router.post('/products', addProduct);
 
 module.exports = router;
