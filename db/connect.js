6eee18b3727e7b1c507d9bea1d2890e66637be64
Ar1f007 <muhammadarif9416@yahoo.com>
2022-08-04 22:38:15 +0600

    initialized the project and connected database

---
+++ db/connect.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const connectDB = (uri) => mongoose.connect(uri);
+module.exports = connectDB;
---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const connectDB = require('./db/connect');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Running fine');
+});
+
+const PORT = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => console.log('Listening to port', PORT));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
